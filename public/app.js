9f4dfbbe909e9e445fda5466a62414bd9236a0e3
serguei9090 <serguei246@gmail.com>
2021-09-10 18:12:19 -0400

    Some test
    Some test

--- public/app.js
+++ public/app.js
@@ -38,13 +38,15 @@ const projectContainer = document.querySelector('.project-container');
 
 projects.forEach(project => {
     projectContainer.innerHTML += `
+    <a class="project-link" href="${project.link}">
     <div class="project-card" data-tags="#all, ${project.tags}">
-        <img src="img/${project.image}" alt="">
-        <div class="content">
+    <img src="img/${project.image}" alt="">
+    <div class="content">
             <h1 class="project-name">${project.name}</h1>
             <span class="tags">${project.tags}</span>
-        </div>
+        </div> 
     </div>
+    </a>
     `;
 })
 // filters
--- public/project.js
+++ public/project.js
@@ -1,13 +1,81 @@
 let projects = [
     {
-        name: "project one",
-        tags: "#javascript, #fullstack, #ui/ux, #backend",
-        image: "project (1).png",
+        name: "Modern Wonders",
+        tags: "#javascript, #fullstack, #WEB, #HTML",
+        image: "1.jpg",
+        link: "#",
     },
     {
-        name: "project two",
+        name: "3RTVCatalog",
+        tags: "#javascript, #fullstack, #WEB, #CSS",
+        image: "1.jpg",
+        link: "#",
+    },
+    {
+        name: "Web Catalog generator",
         tags: "#javascript, #fullstack",
-        image: "project (2).png",
+        image: "1.jpg",
+        link: "#",
+    },
+    {
+        name: "Casa Morejon",
+        tags: "#javascript, #fullstack, #building #RentalHome",
+        image: "1.jpg",
+        link: "#",
+    },
+    {
+        name: "Forms & Buttons Test",
+        tags: "#javascript, #fullstack",
+        image: "1.jpg",
+        link: "#",
+    },
+    {
+        name: "Get Clients Info From JS",
+        tags: "#javascript, #fullstack",
+        image: "1.jpg",
+        link: "#",
+    },
+    {
+        name: "Particle JS Test",
+        tags: "#javascript, #fullstack, #building #RentalHome",
+        image: "1.jpg",
+        link: "#",
+    },
+    {
+        name: "To-Do-List",
+        tags: "#javascript, #fullstack",
+        image: "1.jpg",
+        link: "#",
+    },
+    {
+        name: "Make Folder List",
+        tags: "#python, #scripting",
+        image: "1.jpg",
+        link: "#",
+    },
+    {
+        name: "Video Editor",
+        tags: "#javascript, #fullstack",
+        image: "1.jpg",
+        link: "#",
+    },
+    {
+        name: "Get Folder Size",
+        tags: "#python, #scripting",
+        image: "1.jpg",
+        link: "#",
+    },
+    {
+        name: "Watermark",
+        tags: "#MS-DOS, #ffmpeg",
+        image: "1.jpg",
+        link: "#",
+    },
+    {
+        name: "Time Cal for ejecution",
+        tags: "#MS-DOS, #scripting",
+        image: "1.jpg",
+        link: "#",
     },
     // +8 more
 ]

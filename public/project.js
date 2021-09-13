6f207105065f2b13e8a6a4631f3f4622f9e1cb66
serguei9090 <serguei246@gmail.com>
2021-09-12 21:59:07 -0400

    Pictures In

--- public/project.js
+++ public/project.js
@@ -2,80 +2,89 @@ let projects = [
     {
         name: "Modern Wonders",
         tags: "#fullStack, #WEB",
-        image: "1.jpg",
+        image: "ModernWonders.png",
         link: "https://modernwondersllc.com/",
     },
     {
         name: "3RTVCatalog",
         tags: "#fullStack, #WEB",
-        image: "1.jpg",
+        image: "3RTV Catalog web.png",
         link: "https://serguei9090.github.io/3RTV-Catalog/3RTVPaket-Novelas.html",
-    },
-    {
-        name: "Web Catalog generator",
-        tags: "#Python, #Scripting, Jinja2",
-        image: "1.jpg",
-        link: "https://github.com/serguei9090/Add_Number_To_Img_Py",
     },
     {
         name: "Casa Morejon",
         tags: "#fullStack, #Rentalhome",
-        image: "1.jpg",
+        image: "Casa_Morejon.png",
         link: "https://serguei9090.github.io/Casa-Morejon/",
+    },
+    {
+        name: "3RTVPaket",
+        tags: "#fullStack, #WEB",
+        image: "3RTV Web.png",
+        link: "https://serguei9090.github.io/3RTVPaket/",
+    },
+    {
+        name: "Web Catalog generator",
+        tags: "#Python, #Scripting, Jinja2",
+        image: "Add_Number_img.png",
+        link: "https://github.com/serguei9090/Add_Number_To_Img_Py",
+    },
+    {
+        name: "Add Number to img",
+        tags: "#Python, #Scripting",
+        image: "Add_Number_img.png",
+        link: "https://github.com/serguei9090/Add_Number_To_Img_Py",
+    },
+    {
+        name: "Get&Put Folder Size",
+        tags: "#Python, #Scripting",
+        image: "Get&Put Folder Size.png",
+        link: "#",
+    },
+    {
+        name: "Get time of ejecution",
+        tags: "#MS-DOS, #Scripting",
+        image: "Get-Time-Bat.png",
+        link: "#",
     },
     {
         name: "Forms & Buttons Test",
         tags: "#javaScript, #fullStack",
-        image: "1.jpg",
+        image: "Forms and Button.png",
         link: "https://serguei9090.github.io/Forms_And_Button/",
     },
-    {
-        name: "Get Clients Info From JS",
-        tags: "#javaScript",
-        image: "1.jpg",
-        link: "https://serguei9090.github.io/Forms_And_Button/",
-    },
+
     {
         name: "Particle JS Test",
         tags: "#javaScript",
-        image: "1.jpg",
+        image: "Particle JS Test.png",
         link: "https://serguei9090.github.io/Particles_JS_Test/",
     },
     {
         name: "To-Do-List",
         tags: "#javaScript",
-        image: "1.jpg",
+        image: "To_Do_List.png",
         link: "https://serguei9090.github.io/To-Do-List/",
     },
     {
         name: "Make Folder List",
         tags: "#Python, #Scripting",
-        image: "1.jpg",
+        image: "Add_Number_img.png",
         link: "#",
     },
     {
         name: "Video Editor",
         tags: "#Python, #Scripting",
-        image: "1.jpg",
+        image: "Add_Number_img.png",
         link: "#",
     },
-    {
-        name: "Get Folder Size",
-        tags: "#Python, #Scripting",
-        image: "1.jpg",
-        link: "#",
-    },
+
     {
         name: "Watermark",
         tags: "#MS-DOS, #FFmpeg, #Scripting",
-        image: "1.jpg",
+        image: "Get-Time-Bat.png",
         link: "#",
     },
-    {
-        name: "Time Cal for ejecution",
-        tags: "#MS-DOS, #Scripting",
-        image: "1.jpg",
-        link: "#",
-    },
+
     // +8 more
 ]
